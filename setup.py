3909d46ecee8e366ab1b241019225e76522e0908
zhuo.meng <mengzhuo1203@gmail.com>
2014-11-10 21:10:33 +0800

    add python 3 support statement

--- setup.py
+++ setup.py
@@ -24,7 +24,9 @@ setup(
                         'Intended Audience :: Developers',
                         'License :: OSI Approved :: BSD License',
                         'Operating System :: OS Independent',
-                        'Programming Language :: Python',
+                        'Programming Language :: Python :: 2.7',
+                        'Programming Language :: Python :: 3.3',
+                        'Programming Language :: Python :: 3.4',
                         'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
                         'Topic :: Software Development :: Libraries :: Python Modules'            ]
 )

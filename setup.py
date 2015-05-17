04a9eb11a72eddbb2935c8edd25536d482cdc0df
zhuo.meng <mengzhuo1203@gmail.com>
2015-05-17 22:11:01 +0800

    update setup

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from setuptools import setup, Command
 
 setup(
         name='SQLAlchemy-FullText-Search',
-        version='0.2.2',
+        version='0.2.3',
         url='https://github.com/mengzhuo/sqlalchemy-fulltext-search',
         license='BSD',
         author='Meng Zhuo, Alejandro Mesa',
